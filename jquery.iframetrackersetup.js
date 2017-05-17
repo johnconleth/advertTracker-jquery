014a09966e85745651c639e0a3cd3bf705b47930
jomcat <jomcat@users.noreply.github.com>
2017-05-17 18:51:12 +0100

    Added basic structure to the setup file

--- jquery.iframetrackersetup.js
+++ jquery.iframetrackersetup.js
@@ -3,5 +3,25 @@
  *
  * Setup file for using iframe click tracker jQuery plugin to track AdSense clicks
  * @author John Conleth (http://JohnConleth.com)
- * @copyright 
+ * @copyright © 2017 John Conleth
+ * @version 1.0
  /*
+
+
+// add iframe tracking to all elements with .adsenseadvert style attached
+$(function() {
+   $('.adsenseadvert iframe').iframeTracker({
+   blurCallback: function(){
+    //$('<div class="alert alert-info">').html('Click on iframe : #' + this._overId).appendTo('#consoleDebug').delay(3000).fadeOut();
+    console.log(this._overId);
+   },
+   overCallback: function(element){
+    this._overId = $(element).parents('.iframetrack').attr('id'); // Saving the iframe wrapper id
+   },
+   outCallback: function(element){
+    this._overId = null; // Reset hover iframe wrapper id
+   },
+   _overId: null
+  });
+});
+ 
