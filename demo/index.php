f8b258bcbb530154819eef08e60eab87959ff67d
jomcat <jomcat@users.noreply.github.com>
2017-05-17 23:35:17 +0100

    Update index.php

--- demo/index.php
+++ demo/index.php
@@ -27,16 +27,6 @@ $(document).ready(function() {
 });
 
 </script>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-10447335-2', 'auto');
-  ga('send', 'pageview');
-
-</script>
 </head>
 <h1>Main test area - jQuery plugin</h1>
 <div id="main">
