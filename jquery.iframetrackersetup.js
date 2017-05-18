47b29b4676ac46737951dfd68d2c94976e0da293
jomcat <jomcat@users.noreply.github.com>
2017-05-18 18:24:02 +0100

    Using event instead of pageview tracking

--- jquery.iframetrackersetup.js
+++ jquery.iframetrackersetup.js
@@ -3,20 +3,15 @@
  *
  * Setup file for using iframe click tracker jQuery plugin to track AdSense clicks
  * @author John Conleth (http://JohnConleth.com)
- * @copyright © 2017 John Conleth
+ * @copyright Â© 2017 John Conleth
  * @version 1.0
  */
-
 
 // add iframe tracking to all elements with .adsenseadvert style attached
 $(function() {
    $('.adsenseadvert iframe').iframeTracker({
    blurCallback: function(){
-    $('<div class="consoledebug">').html('Click on iframe : #' + this._overId).appendTo('#status');
-    console.log(this._overId);
-    ga('send', 'pageview', 'advert-'+this._overId);
-
-    //console.log(this);
+    ga('send', 'event', 'Advert', 'click', this._overId);
    },
    overCallback: function(element){
     this._overId = $(element).parents('.adsenseadvert').attr('id'); // Saving the iframe wrapper id
